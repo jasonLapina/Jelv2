86f0baf521c96143705ab238736e2b6fcf7ff03e
jasonLapina <lapina.jason@gmail.com>
2023-10-28 05:22:43 +0800

    footer

--- src/Home/Contact.js
+++ src/Home/Contact.js
@@ -1,18 +1,30 @@
-import { Heading, VStack } from "@chakra-ui/react";
+import { Box, Heading, VStack, Text, Center } from "@chakra-ui/react";
 import Btn from "../Shared/Btn";
 import Highlighted from "../Shared/Highlighted";
+import { ArrowUpIcon } from "@chakra-ui/icons";
 function Contact() {
   return (
-    <VStack gap='24px' bgColor='var(--primary)' p='16px' pb='160px'>
-      <Heading textAlign='center' mt='120px'>
-        Ready to <Highlighted>level up</Highlighted> your marketing without
-        breaking a sweat?
-        <br />
-        Let the <Highlighted>copy </Highlighted> do the{" "}
-        <Highlighted> heavy lifting</Highlighted>
-      </Heading>
-      <Btn>Let jel do the copy</Btn>
-    </VStack>
+    <Box p='16px' bgColor='var(--primary)'>
+      <VStack gap='40px' pb='160px'>
+        <Heading lineHeight='taller' textAlign='center' mt='120px'>
+          Ready to <Highlighted>level up</Highlighted> your marketing without
+          breaking a sweat?
+          <br />
+          Let the <Highlighted>copy </Highlighted> do the{" "}
+          <Highlighted> heavy lifting</Highlighted>
+        </Heading>
+        <Btn>Let jel do the copy</Btn>
+      </VStack>
+      <Text>&copy; JmgWrites.com</Text>
+      <Center>
+        <Text
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+          cursor='pointer'
+        >
+          Back to top <ArrowUpIcon />
+        </Text>
+      </Center>
+    </Box>
   );
 }
 
--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -1,4 +1,4 @@
-import { Heading, Text, VStack } from "@chakra-ui/react";
+import { Box, Grid, Heading, Text, VStack, Image } from "@chakra-ui/react";
 import Btn from "../Shared/Btn";
 import Highlighted from "../Shared/Highlighted";
 import Contact from "./Contact";
@@ -13,18 +13,27 @@ function HomePage() {
         bgColor='var(--primary)'
         p='16px'
         pl='80px'
-        pb='160px'
       >
-        <Heading mt='120px'>
-          Hi there, it's Jel. <br />
-          I’m here to put <Highlighted>words to work.</Highlighted>
-        </Heading>
-        <Text maxW='600px' mb='40px' opacity='.6'>
-          I help e-commerce brands turn subscribers into paying customers and
-          brand loyalists by writing copy that is congruent with their branding,
-          conversion-focused, and community-driven.
-        </Text>
-        <Btn>See my work</Btn>
+        <Grid
+          pt='120px'
+          pb='80px'
+          alignItems='center'
+          gridTemplateColumns='1fr 1fr'
+        >
+          <Box>
+            <Heading mb='24px'>
+              Hi there, it's Jel. <br />
+              I’m here to put <Highlighted>words to work.</Highlighted>
+            </Heading>
+            <Text lineHeight='taller' maxW='600px' mb='40px' opacity='.6'>
+              I help e-commerce brands turn subscribers into paying customers
+              and brand loyalists by writing copy that is congruent with their
+              branding, conversion-focused, and community-driven.
+            </Text>
+            <Btn>See my work</Btn>
+          </Box>
+          <Image justifySelf='center' alignSelf='center' src='/hero.svg' />
+        </Grid>
       </VStack>
       <About />
       <Samples />
