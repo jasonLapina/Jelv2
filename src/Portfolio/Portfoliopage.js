9a05061dc88b2d62cee5d6633b178055eb33cc43
jasonLapina <lapina.jason@gmail.com>
2023-10-29 10:48:37 +0800

    portfolio page

--- src/Portfolio/Portfoliopage.js
+++ src/Portfolio/Portfoliopage.js
@@ -1,6 +1,52 @@
-import { Box } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  Heading,
+  Image,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 function Portfoliopage() {
-  return <Box>porf</Box>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const samples = [
+    "Apparel",
+    "Bedbugs",
+    "BuzzBud",
+    "fruits",
+    "Gassy",
+    "SweetDreams",
+  ];
+  return (
+    <>
+      <Box>
+        <Text opacity='.6' fontSize='24px' mb='40px' textAlign='center'>
+          Portfolio showcase
+        </Text>
+        <Box textAlign='center'>
+          <Heading fontWeight='semibold' mb='24px'>
+            Samples
+          </Heading>
+          <Text maxW='600px' mx='auto' opacity='.6'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Perspiciatis, optio ad dignissimos asperiores id vel mollitia beatae
+          </Text>
+
+          <Grid
+            columnGap='16px'
+            rowGap='32px'
+            gridTemplateColumns='repeat(auto-fit,minmax(400px,1fr))'
+            mt='40px'
+          >
+            {samples.map((s) => (
+              <Box maxH='600px' overflow='hidden' maxW='400px' key={s}>
+                <Image src={`/${s}.jpg`} />
+              </Box>
+            ))}
+          </Grid>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default Portfoliopage;
